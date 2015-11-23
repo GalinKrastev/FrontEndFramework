1f0e565939e8df90585e383c585d95ee3e9c5844
Galin <galinkrastev@gmail.com>
2015-11-23 16:37:10 +0200

    Initial commit

---
+++ Scripts/Framework/framework.js
@@ -0,0 +1,47 @@
+﻿window.framework = window.framework || new function () {
+    this.controllers = {};
+    this.services = {};
+    this.models = {};
+    var that = this;
+
+
+    this.factory = new function objectsFactory() {
+
+        var defaultObjects = {
+            controller: (function (name, object) {
+                // encapsulated this way you cant extend default object
+                // maybe it is a good idea to make the deault objects extensible
+                function controller(name, object) {
+                    this.specifics = {
+                        name: name
+                    };
+                    this.observables = {};
+
+                    if ("specifics" in object)
+                        throw new Error(["cant use property specifics while initializing new controller"]);
+
+                    this.extend(object);
+                }
+                controller.prototype = {
+                    constructor: controller,
+                    index: function () {
+                        throw new Error("Overwriting this method is obligatory.");
+                    },
+                    update: function () {
+                    }
+                }
+
+                return controller;
+            }())
+        }
+
+        return {
+            controller: function (name, object) {
+                if (name.contains("Controller")) {
+                    that.controllers[name] = new defaultObjects.controller(name, object);
+                    return that.controllers[name];
+                }
+            }
+        }
+    }
+};
---
+++ Scripts/Framework/prototypeExtensions.js
@@ -0,0 +1,128 @@
+﻿/*
+ * Object extenshions
+ */
+(!Object.prototype.getType && (Object.prototype.getType = function getType(propertyOrFunction, isOwnProperty) {
+    if (!propertyOrFunction && propertyOrFunction !== 0) {
+        throw new Error([
+            "Parameter propertyOrFunction is required ",
+            "and cannot be null, undefined or false"
+        ]);
+    }
+
+    if (this.type) {
+        return this.type;
+    }
+
+    if (
+        isOwnProperty === true &&
+        this.prototype[propertyOrFunction]
+    ) {
+        return;
+    }
+
+    return typeof this[propertyOrFunction];
+}));
+
+(!Object.prototype.extendWith && (Object.prototype.extendWith = function extend(object) {
+    if (typeof object !== "object") {
+        throw new Error(["Type of object parameter is not an object."]);
+    }
+
+    for (var propName in object) {
+        if (
+            object.hasOwnProperty(propName) && 
+            object[propName] !== null && 
+            object[propName] !== undefined
+        ) {
+            switch (object[propName].constructor) {
+                case Number:
+                case String:
+                case Boolean:
+                case Symbol:
+                    this[propName] = object[propName];
+                    break;
+
+                case Function:
+                    this[propName] = object[propName];
+                    break;
+
+                default: //object
+                    this[propName] = this[propName] ?
+                        this[propName].extend(object[propName]) :
+                        {}.extend(object[propName]);
+                    break;
+            }
+        }
+    }
+
+    if (object.prototype) {
+        this.prototype.extend(object.prototype);
+    }
+
+    return this;
+}));
+
+(!Object.prototype.tryToSetProperties && (Object.prototype.tryToSetProperties = function tryToSetProperties(objWithProps) {
+    for (var prop in objWithProps) {
+        var addProps = true;
+        if (
+            objWithProps.hasOwnProperty(prop) &&
+            !this[prop] === undefined
+        ) {
+            addProps = false;
+        }
+    }
+
+    if (addProps) {
+        this.extend(objWithProps);
+    }
+
+    return addProps;
+}));
+
+(!Object.prototype.toArray && (Object.prototype.toArray = function () {
+    var array = [];
+
+    for (var p in this)
+        if(this.hasOwnProperty(p))
+            array.push(this[p])
+
+    return array;
+}));
+
+/*
+ * String extensions
+ */
+(!String.prototype.contains && (String.prototype.contains = function (text) {
+    return this.indexOf(text) > -1;
+}));
+
+String.prototype.toDashSeparated = function () {
+    var letter, newString = [];
+    for (var i = 0, len = this.length; i < len; i++) {
+        letter = this.charAt(i);
+        newString.push(
+			letter === letter.toLowerCase() ?
+            letter :
+				"-" + letter.toLowerCase()
+		);
+    }
+
+    return newString.join("");
+}
+
+String.prototype.toCamelCase = function () {
+    var letter, newString = [];
+
+    for (var i = 0, len = this.length; i < len; i++) {
+        letter = this.charAt(i);
+
+        if (letter === "-") {
+            letter = this.charAt(++i).toUpperCase();
+        }
+
+        newString.push(letter);
+    }
+
+    return newString.join("");
+}
